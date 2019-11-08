0dec0751b82bd41d9ec0e74d0140d8a18cee5e43
Borislava <borislava_angelova_95@abv.bg>
2019-11-08 22:52:56 +0200

    Connection to the DB.

--- fcc-car-insurance/src/app/app.module.ts
+++ fcc-car-insurance/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 import { CarComponentComponent } from './car-component/car-component.component';
 import { PartComponent } from './part/part.component';
 
@@ -14,6 +18,8 @@ import { PartComponent } from './part/part.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'fcc-car-insurance'),
+    AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
--- fcc-car-insurance/src/app/part/part.component.ts
+++ fcc-car-insurance/src/app/part/part.component.ts
@@ -1,22 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
+class CarPart
+{
+    partName: string;
+    partPictureUrl: string
+    
+    constructor( partName:string, partPictureUrl:string)
+    {
+      this.partName = partName;
+      this.partPictureUrl = partPictureUrl;
+    }
+}
 
 @Component({
   selector: 'app-part',
   templateUrl: './part.component.html',
   styleUrls: ['./part.component.scss']
 })
+
 export class PartComponent implements OnInit {
   route: ActivatedRoute;
-  carPart:String;
+  carPartName:string;
+  carPartPictureUrl:string;
+  carPartsFiltered: Observable<CarPart[]>;
+  db: AngularFireDatabase;
+  carPartsListDB:AngularFireList<CarPart>;
 
-  constructor(router: ActivatedRoute) {
+  constructor(router: ActivatedRoute, db: AngularFireDatabase) {
     this.route = router;
-   }
-
-  ngOnInit() {
-    this.carPart = this.route.snapshot.paramMap.get('id');
+    this.db = db;
   }
 
+  ngOnInit() {
+    this.carPartName = this.route.snapshot.paramMap.get('id');
 
+    this.carPartName = this.carPartName.toLowerCase();
+
+    if(this.carPartName == "circle_006")
+    {
+      this.carPartName = "circle_002";
+    }
+    else if(this.carPartName == "circle_012")
+    {
+      this.carPartName = "circle_011";
+    }
+    else if(this.carPartName.includes("carosserie"))
+    {
+      this.carPartName = "carosserie";
+    }
+
+    this.carPartsFiltered = this.db.list<CarPart>("/parts",
+                          part => part.orderByChild('partName').equalTo(this.carPartName).limitToFirst(1)).valueChanges();
+  }
 }
